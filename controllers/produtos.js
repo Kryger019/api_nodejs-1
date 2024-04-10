c0b7d12ca0c0bce8246a1b200017cc67242f7286
David Vanjão <35746587+davidvanjao@users.noreply.github.com>
2024-04-09 21:15:20 -0300

    ok

--- controllers/produtos.js
+++ controllers/produtos.js
@@ -4,62 +4,114 @@ const db = require('../database/connection');
 module.exports = {
     async listarProdutos(request, response) {
         try {
+
+            // instruções SQL
+            const sql = `select prd_id, prd_nome, prd_valor, prd_unidade, ptp_id, 
+                prd_disponivel, prd_img, prd_destaque, prd_img_destaque, prd_descricao 
+                from produtos;`; 
+            // executa instruções SQL e armazena o resultado na variável usuários
+            const produtos = await db.query(sql); 
+            const nItens = produtos[0].length;
+
             // throw new Error('Eu causei o erro!');
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Lista de produtos.', 
-                dados: null
+                dados: produtos[0], 
+                nItens    
             });
+
         } catch (error) {
             return response.status(500).json({
                 sucesso: false, 
                 mensagem: `Erro na requisição. -${error}`, 
-                dados: null
+                dados: error.message
             });
         }
     }, 
+
     async cadastrarProdutos(request, response) {
         try {
+
+            // parâmetros recebidos no corpo da requisição
+            const { prd_nome, prd_valor, prd_unidade, ptp_id, prd_disponivel, prd_img, prd_destaque, prd_img_destaque, prd_descricao } = request.body;
+            // instrução SQL
+            const sql = `INSERT INTO produtos 
+                (prd_nome, prd_valor, prd_unidade, ptp_id, prd_disponivel, prd_img, prd_destaque, prd_img_destaque, prd_descricao) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+            // definição dos dados a serem inseridos em um array
+            const values = [prd_nome, prd_valor, prd_unidade, ptp_id, prd_disponivel, prd_img, prd_destaque, prd_img_destaque, prd_descricao];  
+            // execução da instrução sql passando os parâmetros
+            const execSql = await db.query(sql, values); 
+            // identificação do ID do registro inserido
+            const pro_id = execSql[0].insertId;
+
             return response.status(200).json({
                 sucesso: true, 
-                mensagem: 'Cadastro de produtos.', 
-                dados: null
+                mensagem: 'Produto cadastrado com sucesso!.', 
+                dados: pro_id
             });
         } catch (error) {
             return response.status(500).json({
                 sucesso: false, 
                 mensagem: `Erro na requisição. -${error}`, 
-                dados: null
+                dados: error.message
             });
         }
     }, 
+
     async editarProdutos(request, response) {
         try {
+            // parâmetros recebidos pelo corpo da requisição
+            const { prd_nome, prd_valor, prd_unidade, ptp_id, prd_disponivel, prd_img, prd_destaque, prd_img_destaque, prd_descricao } = request.body;
+            // parâmetro recebido pela URL via params ex: /usuario/1
+            const { prd_id } = request.params; 
+            // instruções SQL
+            const sql = `UPDATE produtos SET prd_nome = ?, prd_valor = ?, prd_unidade = ?, ptp_id = ?,
+                prd_disponivel = ?, prd_img = ?, prd_destaque = ?, prd_img_destaque = ?, prd_descricao = ?
+                WHERE prd_id = ?;`; 
+            // preparo do array com dados que serão atualizados
+            const values = [prd_nome, prd_valor, prd_unidade, ptp_id, prd_disponivel, prd_img, prd_destaque, prd_img_destaque, prd_descricao, prd_id]; 
+            // execução e obtenção de confirmação da atualização realizada
+            const atualizaDados = await db.query(sql, values); 
+
             return response.status(200).json({
                 sucesso: true, 
-                mensagem: 'Editar produtos.', 
-                dados: null
+                mensagem: `Produto ${prd_id} atualizado com sucesso!`, 
+                dados: atualizaDados[0].affectedRows 
             });
+
         } catch (error) {
             return response.status(500).json({
                 sucesso: false, 
                 mensagem: `Erro na requisição. -${error}`, 
-                dados: null
+                dados: error.message
             });
         }
     }, 
+
+
     async apagarProdutos(request, response) {
         try {
+            // parâmetro passado via url na chamada da api pelo front-end
+            const { prd_id } = request.params;
+            // comando de exclusão
+            const sql = `DELETE FROM produtos WHERE prd_id = ?`;
+            // array com parâmetros da exclusão
+            const values = [prd_id];
+            // executa instrução no banco de dados
+            const excluir = await db.query(sql, values);
+
             return response.status(200).json({
                 sucesso: true, 
-                mensagem: 'Apagar produtos.', 
-                dados: null
+                mensagem: `Produto ${prd_id} excluído com sucesso`,
+                dados: excluir[0].affectedRows
             });
         } catch (error) {
             return response.status(500).json({
                 sucesso: false, 
                 mensagem: `Erro na requisição. -${error}`, 
-                dados: null
+                dados: error.message
             });
         }
     }, 
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -86,20 +86,87 @@ module.exports = {
             });
         }
     }, 
+
     async apagarUsuarios(request, response) {
         try {
+            // parâmetro passado via url na chamada da api pelo front-end
+            const { usu_id } = request.params;
+            // comando de exclusão
+            const sql = `DELETE FROM usuarios WHERE usu_id = ?`;
+            // array com parâmetros da exclusão
+            const values = [usu_id];
+            // executa instrução no banco de dados
+            const excluir = await db.query(sql, values);
+
             return response.status(200).json({
-                sucesso: true, 
-                mensagem: 'Apagar usuários.', 
-                dados: null
+                sucesso: true,
+                mensagem: `Usuário ${usu_id} excluído com sucesso`,
+                dados: excluir[0].affectedRows
             });
         } catch (error) {
             return response.status(500).json({
-                sucesso: false, 
-                mensagem: `Erro na requisição. -${error}`, 
-                dados: null
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
             });
         }
     }, 
+    
+    async ocultarUsuario(request, response) {
+        try {
+            const usu_ativo = false; 
+            const { usu_id } = request.params; 
+            const sql = `UPDATE usuarios SET usu_ativo = ? 
+                WHERE usu_id = ?;`;
+            const values = [usu_ativo, usu_id]; 
+            const atualizacao = await db.query(sql, values); 
+            
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: `Usuário ${usu_id} excluído com sucesso`,
+                dados: atualizacao[0].affectedRows
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+    async login(request, response) {
+        try {
+
+            const { usu_email, usu_senha } = request.body;
+
+            const sql = `SELECT usu_id, usu_nome, usu_tipo FROM usuarios 
+                WHERE usu_email = ? AND usu_senha = ? AND usu_ativo = 1;`;
+
+            const values = [usu_email, usu_senha];
+
+            const usuarios = await db.query(sql, values);
+            const nItens = usuarios[0].length; 
+
+            if (nItens < 1) {
+                return response.status(403).json({
+                    sucesso: false,
+                    mensagem: 'Login e/ou senha inválido.',
+                    dados: null,
+                });
+            }
+            // fazendo login
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Login efetuado com sucesso',
+                dados: usuarios[0]
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }
 }
 
